f3ca03fffbdca0c7ce3541ebfa0cc18cbd32bab9
Grigoriy <foxtail25@gmail.com>
2022-11-11 00:22:06 +0300

    DZ

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // import { dblClick } from '@testing-library/user-event/dist/click';
 // import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+
 // import { Header } from './AppHeader/AppHeader';
+
 import './index.css';
 import App from './components/App/app';
 
